c793eeba942c5445f3e77a970a9fb74b82083733
Anil <anil.acharya6851@gmail.com>
2019-06-25 14:13:34 -0400

    image parser utility added and linked with the asynchronous processor

--- AsynchronousProcessor.py
+++ AsynchronousProcessor.py
@@ -1,6 +1,8 @@
 
-
+from ParserUtility import *
 def processAsynchronously(workerName,queue):
    while(1):
      stream= queue.get(block=True)
+     imageByte= parseImage(stream.image)
+     #fileName=buildFileName(stream.date, stream.deviceId)
      print(stream)
---
+++ ParserUtility.py
@@ -0,0 +1,15 @@
+import base64
+import numpy as np
+from datetime import datetime
+import cv2
+def parseImage(img):
+ currentImage = base64.b64decode(img);
+ nparr = np.fromstring(currentImage, np.uint8)
+ img_np = cv2.imdecode(nparr, 1)
+ ret, jpegImage = cv2.imencode('.jpg', img_np)
+ return jpegImage.tobytes()
+
+
+def buildFileName(createdDate, deviceName):
+     date_time = createdDate.strftime("%m/%d/%Y-%H:%M:%S")
+     return deviceName+ date_time
