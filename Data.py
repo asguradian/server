7d5496c36ff8fe6a8a938be5db4e45936fae05e8
Anil <anil.acharya6851@gmail.com>
2019-06-26 09:58:43 -0400

    Data type contract updated

--- Data.py
+++ Data.py
@@ -1,5 +1,6 @@
 class Stream:
- def __init__(self,image, date,deviceId):
+ def __init__(self,image, date,deviceId,fileName):
    self.image=image;
    self.date=date;
    self.deviceId=deviceId;
+   self.fileName=fileName
